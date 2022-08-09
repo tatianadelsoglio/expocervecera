4e2d5ce3db425ab19855b085d47c147d95fd9700
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-08-09 17:13:53 -0300

    se agrego color y links del menu

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
 import "./App.css";
+import logo from "./logoExpoCe.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="header">
-        <div className="div-logo">logo</div>
-        <div className="div-menu">
-          <a>Novedades</a>
-          <a>Sponsors</a>
-          <a>Ubicacion</a>
-          <a>Contacto</a>
+        <div className="header_content">
+          <div className="div-logo">
+            <img src={logo} width="250px" height="90px" alt="logo-expoCe"/>
+          </div>
+          <div className="div-menu">
+            <a href="#novedades">Novedades</a>            
+            <a href="#ubicacion">Ubicación</a>
+            <a href="#sponsors">Sponsors</a>
+            <a href="#galeria">Galeria</a>
+            <a href="#contacto">Contacto</a>
+          </div>
         </div>
       </header>
       <body className="body">
-        <section className="seccion-sponsors">
-          <div className="mitad"></div>
-          <div className="mitad"></div>
+        <section className="novedades" id="novedades">
+          <div className="mitad">Novedades 1</div>
+          <div className="mitad">Novedades 2</div>
         </section>
-        <section className="seccion-informacion">
-          <div className="mitad"></div>
-          <div className="mitad"></div>
+        <section className="ubicacion" id="ubicacion">
+          <div className="mitad">Ubicacion Texto</div>
+          <div className="mitad">Ubicación Mapa</div>
+        </section>
+        <section className="sponsors" id="sponsors">
+          <div className="div_sponsors">SPONSORS</div>
+        </section>
+        <section className="galeria" id="galeria">
+          <div className="mitad">Galeria</div>
+          <div className="mitad">Galeria</div>
         </section>
       </body>
       <footer className="footer">
-        <div className="contacto">
+        <div className="contacto" id="contacto">
           <p>Email: asdgasdga@cerveza.com</p>
           <p>Telefono: 3535512412</p>
           <p>Ruta 158 km 237</p>
