843d56d820b1cd35516c038e3f85a49fb0716b39
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-08-12 18:19:50 -0300

    se probo contactos.php

--- contacto.php
+++ contacto.php
@@ -1,37 +1,20 @@
 <?php
-
-// if(isset($_POST['email'])) {
-
-//     // Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
-//     $email_to = "tatianadelsoglio96@gmail.com";
-//     $email_subject = "Contacto desde el sitio web";
-    
-//     // Aquí se deberían validar los datos ingresados por el usuario
-//     if(!isset($_POST['nombre']) ||
-// //      !isset($_POST['telefono']) ||
-// //      !isset($_POST['email']) ||
-// //      !isset($_POST['mensaje'])) {
-    
-//     echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
-//     echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
-//     die();
-//     }
-
+   if (isset ($_POST['enviar'])) {
 
     $nombre = $_POST['nombre'];
     $telefono = $_POST['telefono'];
     $email = $_POST['email'];
     $mensaje = $_POST['mensaje'];
 
-    $to = "info@expocervecera.com.ar";
+
+    $to = "tatianadelsoglio96@gmail.com";
     $subject = "Contacto desde el sitio web";
 
     $headers = 'From: '.$email."\r\n";
     $headers .= 'Reply-To: '.$email."\r\n";
-    $headers .= "MIME-Version: 1.0 \r\n";
     $headers .= 'X-Mailer: PHP/' . phpversion();
 
-    
+    // $message = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje"
 
 
     $message = "Detalles del formulario de contacto:\n\n";
@@ -41,24 +24,14 @@
     $message .= "Comentarios: " . $mensaje . "\n\n";
 
     
-
-    mail($to, $subject, utf8_decode($message), $headers);
-
-    echo "¡El mensaje se ha enviado con éxito!";
-
-    // header("Location:index.html");
-
-    // Ahora se envía el e-mail usando la función mail() de PHP
-
-    // if() {
-    //     echo "Mensaje Enviado";
-    // } else {
-    //     echo "Error, al enviar mensaje";
-    // }
+ 
+        if (mail($to, $subject, $message, $headers)) {
+            echo 'El mensaje se ha enviado';
+        } else {
+            echo 'Falló el envio';
+        }
 
 
-    // echo "¡El formulario se ha enviado con éxito!";
-
-//}
+    };
 
 ?>
